58f65f0574f321c4fa4a95127a85142830d0f3c0
Dekroo <dekroostream@gmail.com>
2025-03-30 16:52:00 +0200

    MadeNavigation

--- app/src/main/java/com/example/next_one/MainActivity.kt
+++ app/src/main/java/com/example/next_one/MainActivity.kt
@@ -4,21 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.next_one.ui.NextApp
 import com.example.next_one.ui.theme.Next_OneTheme
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
+import com.example.next_one.ui.NextAppStartingScreen
+import com.example.next_one.ui.AboutScreen
+import com.example.next_one.ui.HowToPlay
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             Next_OneTheme {
-
+                NavHost(navController = navController, startDestination = "start_Screen") {
+                    composable("start_Screen") {
+                        NextAppStartingScreen(navController)
+                    }
+                    composable("game_Screen") {
+                        NextApp()
+                    }
+                    composable("htp_Screen") {
+                        HowToPlay()
+                    }
+                    composable("about_Screen") {
+                        AboutScreen()
+                    }
+                }
+//            NextApp()
             }
         }
     }
 }
-
